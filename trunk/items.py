0601aab92935b95bc027b90ee36eaaf66122a1af
xergio <xergio@2640be0e-1013-0410-95bc-ce30011b7d25>
2006-05-18 21:45:51 +0000

    Added a link to feed (CFeed) for each item.

--- trunk/items.py
+++ trunk/items.py
@@ -5,8 +5,9 @@ from durus.persistent import Persistent
 
 
 class CItem(Persistent):
-    def __init__(self, title = '', text = '', plink = '', dte = ''):
+    def __init__(self, title = '', text = '', plink = '', dte = '', feed = None):
         self.title = title
         self.text = text
         self.permalink = plink
         self.date = dte
+        self.feed = feed
