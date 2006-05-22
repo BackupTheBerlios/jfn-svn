4bb7d3acc5a9a8aff2f68ea7233cb11c8b5278cf
xergio <xergio@2640be0e-1013-0410-95bc-ce30011b7d25>
2006-05-22 22:37:03 +0000

    Added functions.

--- trunk/users.py
+++ trunk/users.py
@@ -17,6 +17,10 @@ class CUser(Persistent):
         self.items_pending = PersistentList() # [CItem, ...]
         self.config = PersistentDict()
         self.feeds = PersistentDict() # {CFeed: send first notification?}
+        
+        
+    def __len__(self):
+        return len(self.feeds)
             
         
     def subs_feed(self, feeditem, sendFirstNoti=False):
@@ -53,6 +57,15 @@ class CUser(Persistent):
     
     def clear_items(self):
         self.items_pending = PersistentList()
+        
+        
+    def setup(self, action, mode):
+        self.config[action] = mode
+        return True
+        
+        
+    def getConfig(self, key):
+        return self.config.get(key)
 
 
 class CUsers(Persistent):
@@ -76,9 +89,13 @@ class CUsers(Persistent):
         
     def __getitem__(self, key):
         return self.data.get(key)
+        
+        
+    def __len__(self):
+        return len(self.data)
     
     
-    def add_feed(self, jid, feed):
+    def add_feed(self, jid, feed=None):
         """Add an user if not exists and subscribe the feed url, if not exists.
         """
         
@@ -86,16 +103,20 @@ class CUsers(Persistent):
 
         if not self.data.get(jid):
             self.data[jid] = CUser(jid)
-        if not self.feeds.get(feed):
+        if not self.feeds.get(feed) and feed:
             self.feeds[feed] = CFeed(feed)
             fn = False
-            
-        oku = self.data[jid].subs_feed(self.feeds[feed], fn)
-        okf = self.feeds[feed].add_user(self.data[jid])
+
+        if feed:
+            oku = self.data[jid].subs_feed(self.feeds[feed], fn)
+            okf = self.feeds[feed].add_user(self.data[jid])
             
         self.save()
         
-        return oku and okf
+        if feed:
+            return oku and okf
+        else:
+            return oku
         
     
     def del_feed(self, jid, feed):
@@ -112,6 +133,46 @@ class CUsers(Persistent):
         self.save()
         
         return oku and okf
+        
+        
+    def notification_method(self, jid):
+        """Return 'how the user will receive the notifications'"""
+        tempuser = self.data.get(jid)
+        if tempuser:
+            hl = tempuser.getConfig('useheadline')
+            if not hl or hl == "on":
+                return "by headlines"
+            return "by chat message"
+        else:
+            return "-"
+        
+    def notification_when(self, jid):
+        """Return 'when the user wants to receive notifications'"""
+        tempuser = self.data.get(jid)
+        if tempuser:
+            oa = tempuser.getConfig('onlyavailable')
+            if not oa or oa == "off":
+                return "always"
+            return "available only, or ready for chat"
+        else:
+            return "-"
+        
+    def len_feeds(self, jid):
+        tempuser = self.data.get(jid)
+        if tempuser:
+            return str(len(tempuser))
+        else:
+            return "0"
+            
+            
+    def setup(self, jid, action, mode):
+        tempuser = self.data.get(jid)
+        if not tempuser:
+            tempuser = CUser(jid)
+            self.data[jid] = tempuser
+        
+        tempuser.setup(action, mode)
+        return True
         
     
     def get(self, key):
